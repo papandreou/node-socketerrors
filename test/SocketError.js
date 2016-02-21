f1c1f11ef895c955877d62a6c37b5d65a7fdb0cd
Andreas Lind <andreas@one.com>
2016-02-21 21:25:10 +0100

    Use the superclass SocketError constructor as the main export.
    Remove the NotSocketError class, instead introduce a supports method that
    can be consulted before creating an instance.

--- test/SocketError.js
+++ test/SocketError.js
@@ -1,18 +1,19 @@
 var expect = require('unexpected');
-var httpErrors = require('httperrors');
+var HttpError = require('httperrors');
 var http = require('http');
 var socketCodesMap = require('../lib/socketCodesMap');
-var socketErrors = require('../lib/socketErrors');
+var SocketError = require('../lib/SocketError');
+// Alias to avoid jshint complaints due to capitalized function used without new:
+var socketErrorConstructorInLowerCase = SocketError;
 
-describe('socketErrors', function () {
-
+describe('SocketError', function () {
     it('will create a properly subclassed instance', function (done) {
         // capture a genuine ECONNREFUSED error
         http.get('http://localhost:59891/').on('error', function (err) {
-            var socketError = socketErrors(err);
-            var httpError = new httpErrors[504]();
+            var socketError = SocketError(err);
+            var httpError = new HttpError[504]();
 
-            expect(socketError, 'to equal', new socketErrors[err.code](err));
+            expect(socketError, 'to equal', new SocketError[err.code](err));
 
             // has the original error propeties
             expect(socketError, 'to have properties', Object.keys(err));
@@ -23,7 +24,7 @@ describe('socketErrors', function () {
             // has named errorCode property
             expect(socketError[err.code], 'to be true');
 
-            expect(socketError, 'to be a', socketErrors.SocketError);
+            expect(socketError, 'to be a', SocketError.SocketError);
 
             done();
         });
@@ -31,21 +32,18 @@ describe('socketErrors', function () {
 
     it('will return unknown error if it was not mapped', function () {
         var err = new Error();
-        var socketError = socketErrors(err);
+        var socketError = SocketError(err);
 
-        expect(socketError, 'to equal', new socketErrors.NotSocketError());
-
-        // has named errorCode property
-        expect(socketError.NotSocketError, 'to be true');
+        expect(socketError, 'to equal', new SocketError());
     });
 
     it('will not alter the original error', function () {
         var err = new Error();
         err.code = 'ECONNREFUSED';
-        var socketError = socketErrors(err);
+        var socketError = SocketError(err);
 
         // assert socketError was altered
-        expect(socketError, 'to equal', new socketErrors[err.code](err));
+        expect(socketError, 'to equal', new SocketError[err.code](err));
 
         // assert orignal err was untouched
         expect(err, 'not to have properties', ['statusCode']);
@@ -59,16 +57,16 @@ describe('socketErrors', function () {
             it('is correctly instantiated', function () {
                 var err = new Error();
                 err.code = errorCode;
-                var socketError = socketErrors(err);
+                var socketError = SocketError(err);
 
-                expect(socketError, 'to equal', new socketErrors[errorCode](err));
+                expect(socketError, 'to equal', new SocketError[errorCode](err));
 
                 // has named errorCode property
                 expect(socketError[errorCode], 'to be true');
             });
 
             it('returns a ' + statusCode, function () {
-                var socketError = socketErrors((function () {
+                var socketError = SocketError((function () {
                     var err = new Error();
                     err.code = errorCode;
                     return err;
@@ -80,17 +78,46 @@ describe('socketErrors', function () {
             it('lets the `code` from the original instance take precedence over the one built into the class', function () {
                 var err = new Error();
                 err.code = 'SOMETHINGELSE';
-                var socketError = socketErrors(err);
+                var socketError = SocketError(err);
 
                 expect(socketError.code, 'to equal', 'SOMETHINGELSE');
             });
 
             describe('when instantiated via the constructor', function () {
                 it('has a `code` property', function () {
-                    expect(new socketErrors[errorCode]().code, 'to equal', errorCode);
+                    expect(new SocketError[errorCode]().code, 'to equal', errorCode);
                 });
             });
         });
     });
 
+    it('has the SocketError superclass constructor as the main export', function () {
+        expect(new SocketError.ECONNREFUSED(), 'to be a', SocketError);
+    });
+
+    it('should create an instance via SocketError(errorCode)', function () {
+        expect(socketErrorConstructorInLowerCase('ECONNREFUSED'), 'to be a', SocketError.ECONNREFUSED);
+    });
+
+    describe('#supports', function () {
+        it('should return true for a mapped socket error code', function () {
+            expect(SocketError.supports('ECONNREFUSED'), 'to be true');
+        });
+
+        it('should return false for an unmapped socket error code', function () {
+            expect(SocketError.supports('FOOBAR'), 'to be false');
+        });
+
+        it('should return true for a unmapped socket error instance', function () {
+            var fakeSocketError = new Error('ECONNREFUSED');
+            fakeSocketError.code = 'ECONNREFUSED';
+            expect(SocketError.supports(fakeSocketError), 'to be true');
+        });
+
+        it('should return false for a unmapped socket error instance', function () {
+            var fakeSocketError = new Error('FOOBAR');
+            fakeSocketError.code = 'FOOBAR';
+            expect(SocketError.supports(fakeSocketError), 'to be false');
+        });
+    });
 });
